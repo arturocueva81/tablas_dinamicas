b43b3614457fa2afb4174e0cdddbc0b3a5c9bd25
arturocueva81 <ajcs81@hotmail.com>
2026-04-23 21:14:43 -0500

    Taller Tablas: parte_5 primer contacto con JS

---
+++ multiplicar.js
@@ -0,0 +1,5 @@
+function generarTablas() {
+    let contenedor = document.getElementById("contenedorTabla");
+    contenedor.innerHTML = "<h1>PROBANDO</h1>";
+    console.log("Sistema Android: Contenido actualizado correctamente.");
+}
